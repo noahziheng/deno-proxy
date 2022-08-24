3d5a0903a5e1f7b7bcde78daca1c1bb707aacfef
Noah Gao <noahgaocn@outlook.com>
2022-08-24 13:00:09 +0800

    feat: syntax

--- index.ts
+++ index.ts
@@ -6,6 +6,5 @@ serve(async (req: Request) => {
     const newReq = req.clone();
     const newUrl = new URL(req.url);
     newUrl.host = TARGET_HOST;
-    newReq.url = newUrl.toString();
     return fetch(newUrl, newReq);
 });
