6294be0f4dc28d6e98ca53f1f9ea5c7b11658873
Noah Gao <noahgaocn@outlook.com>
2022-08-24 13:07:46 +0800

    feat: add debug

--- index.ts
+++ index.ts
@@ -3,13 +3,10 @@ import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 const TARGET_HOST = Deno.env.get('TARGET_HOST');
 
 serve(async (req: Request) => {
-    const isDebug = req.headers['x-debug'] === 'true';
     const newReq = req.clone();
     const newUrl = new URL(req.url);
     newUrl.host = TARGET_HOST;
     const resp = fetch(newUrl, newReq);
-    if (isDebug) {
-        console.log(req, resp);
-    }
+    console.log(req, resp);
     return resp;
 });
